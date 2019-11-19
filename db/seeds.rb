e003e0531d9f119c13cfb5a0604952fbd36aef96
Alexandre BARAT <alex.barat@gmail.com>
2019-11-19 16:47:59 +0100

    Views for owner show and destroy

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,8 @@ puts 'Creating user joe'
 User.create!(email: "joe@gmail.com", password: "azerty")
 
 puts 'Creating courses...'
-Course.create!(title: 'Cours de skate', description: 'Je compte faire deux heures de skate dimanche sur les quais et je propose de donner des cours pendant 1h', price: 3, duration: 60, location: 'Bordeaux, Quais des Chartrons', date: Time.now, user_id: 1 )
-Course.create!(title: 'Cours de cuisine', description: 'Propose une initiation à la cuisin moléculaire', price: 4, duration: 120, location: 'Bordeaux, Quai Richelieu', date: Time.now, user_id: 1 )
-Course.create!(title: 'Faire sa propre lessive', description: 'Je vais faire ma lessive du mois, je vous propose de vous apprendre par la même occasion comment faire', price: 2, duration: 30, location: 'Bordeaux, Rue Sainte-Catherine', date: Time.now, user_id: 1 )
+Course.create!(title: 'Cours de skate', description: 'Je compte faire deux heures de skate dimanche sur les quais et je propose de donner des cours pendant 1h', price: 3, duration: 60, location: 'Bordeaux, Quais des Chartrons', date: Time.now, user: User.first )
+Course.create!(title: 'Cours de cuisine', description: 'Propose une initiation à la cuisin moléculaire', price: 4, duration: 120, location: 'Bordeaux, Quai Richelieu', date: Time.now, user: User.first )
+Course.create!(title: 'Faire sa propre lessive', description: 'Je vais faire ma lessive du mois, je vous propose de vous apprendre par la même occasion comment faire', price: 2, duration: 30, location: 'Bordeaux, Rue Sainte-Catherine', date: Time.now, user: User.first )
 
 puts 'Finished!'
