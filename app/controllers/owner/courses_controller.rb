43187c5b06c725e427a02f37fef1a2aa78942dce
Alexandre BARAT <alex.barat@gmail.com>
2019-11-19 15:45:53 +0100

    owner controller cleaning

--- app/controllers/owner/courses_controller.rb
+++ app/controllers/owner/courses_controller.rb
@@ -1,8 +1,10 @@
 class Owner::CoursesController < ApplicationController
+  before_action :set_course, only: [:edit, :update, :show, :destroy]
+
   def index
     @courses = current_user.courses
   end
-  
+
   def new
     user = current_user
     @course = Course.new
@@ -13,30 +15,30 @@ class Owner::CoursesController < ApplicationController
     @course.save
     redirect_to owner_courses_path(@course.id)
   end
-  
+
   def edit
-    @course = Course.find(params['id'])
   end
 
   def update
-    @course = Course.find(params['id'])
     @course.update(course_params)
     redirect_to ownr_course_path(@course.id)
   end
 
   def show
-    @course = Course.find(params[:id])
   end
 
   def destroy
-    @course = Course.find(params[:id])
     @course.destroy
     redirect_to owner_courses_path
   end
 
   private
 
+  def set_course
+    @course = Course.find(params[:id])
+  end
+
   def course_params
-    params.require(:course).permit(:title, :description, :price, :duration, :location, :date, :user_id
+    params.require(:course).permit(:title, :description, :price, :duration, :location, :date, :user_id)
   end
 end
