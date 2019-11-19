a086567e93ba80b81f43ebc5e64c4b9e8ed7d3c5
balthazar lebret <balthazarlb@gmail.com>
2019-11-19 15:13:50 +0100

    methods edit and update done

--- app/controllers/owner/courses_controller.rb
+++ app/controllers/owner/courses_controller.rb
@@ -1,2 +1,11 @@
 class Owner::CoursesController < ApplicationController
+  def edit
+    @course = Course.find(params['id'])
+  end
+
+  def update
+    @course = Course.find(params['id'])
+    @course.update(course_params)
+    redirect_to ownr_course_path(@course.id)
+  end
 end
