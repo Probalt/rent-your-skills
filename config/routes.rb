54ce51394aeac98e7183632c692a1bcf2b61345d
Arthur Menard <arthurmenard12@gmail.com>
2019-11-19 11:58:33 +0100

    Routes on the good way

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :owner do
+    resources :courses
+  end
+  resources :courses, only: [:index, :show] do
+    resources :bookings, only: [:create, :index, :show, :destroy]
+  end
 end
