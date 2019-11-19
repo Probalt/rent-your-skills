a96e6d13b342fdc1c5bae77f5889c377dbea1f59
Johanna Heilles <johanna.heilles@hotmail.fr>
2019-11-19 15:11:53 +0100

    new and create

--- app/controllers/owner/courses_controller.rb
+++ app/controllers/owner/courses_controller.rb
@@ -1,2 +1,18 @@
 class Owner::CoursesController < ApplicationController
+  def new
+    user = current_user
+    @course = Course.find(params[:user_id])
+  end
+
+  def create
+    @course = Course.new(course_params)
+    @course.save
+    redirect_to owner_courses_path
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:title, :description, :price, :duration, :location, :date, :user_id)
+  end
 end
