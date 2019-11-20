81e83d12347894093624e2fbafaee35ba4c409f3
Johanna Heilles <johanna.heilles@hotmail.fr>
2019-11-20 12:50:04 +0100

    courses show and booking create

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,14 @@
 class BookingsController < ApplicationController
+  def new
+    @user = current_user
+    @booking = Booking.new
+  end
+
+  def create
+    @user = current_user
+    @course = Course.find(params[:course_id])
+    @booking = Booking.new(user: @user, course: @course)
+    @booking.save
+    redirect_to course_bookings_path
+  end
 end
