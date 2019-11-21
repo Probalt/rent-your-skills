9725fdc39f1ed530c531c37a02f008b1c35f961c
Arthur Menard <arthurmenard12@gmail.com>
2019-11-21 20:16:10 +0100

    Search bar without css ok

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,14 +2,37 @@ class CoursesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @courses = Course.geocoded.all
-    @markers = @courses.map do |course|
+    # @courses = Course.geocoded.all
+    # @markers = @courses.map do |course|
+    #   {
+    #     lat: course.latitude,
+    #     lng: course.longitude,
+    #     infoWindow: render_to_string(partial: "info_window", locals: { course: course }),
+    #     photo: helpers.asset_url('pin.png')
+    #   }
+    # end
+
+    if params[:query].present?
+      sql_query = "title ILIKE :query OR address ILIKE :query"
+      @courses = Course.geocoded.where(sql_query, query: "%#{params[:query]}%")
+      @markers = @courses.map do |course|
       {
         lat: course.latitude,
         lng: course.longitude,
         infoWindow: render_to_string(partial: "info_window", locals: { course: course }),
         photo: helpers.asset_url('pin.png')
       }
+      end
+    else
+      @courses = Course.geocoded.all
+      @markers = @courses.map do |course|
+      {
+        lat: course.latitude,
+        lng: course.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { course: course }),
+        photo: helpers.asset_url('pin.png')
+      }
+      end
     end
   end
 
