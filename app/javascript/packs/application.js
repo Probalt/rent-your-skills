d1df3009d4fbc32591d2fa5ac92ec3361abe8f55
Johanna Heilles <johanna.heilles@hotmail.fr>
2019-11-21 16:28:38 +0100

    homepage navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,9 @@
 import "bootstrap";
+
+if (window.location.pathname !== "/") {
+  const navbar = document.querySelector(".navbar-skills");
+  navbar.style.backgroundColor = "white";
+  navbar.style.position = "static";
+  const image = document.querySelector(".navbar-skills img");
+  image.src = "https://res.cloudinary.com/dcxgnsvxv/image/upload/v1574349676/txuczq2cr3q9c6kyjmpj.png";
+}
